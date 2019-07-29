e329042cb873ea25ba30b80a84eaadfe8e15cede
Colby Nelson <cnelson27@outlook.com>
2019-07-29 07:23:58 -0600

    Stubbed in Navbar method and DOM rendering

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Navbar() {
+  return (
+    <div class="navContainer">
+      <ul class="mainNavContainer">
+
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
