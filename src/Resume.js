cb45c15bddcb63665d2292a6a62c481b5e73c93b
Colby Nelson <cnelson27@outlook.com>
2019-11-02 12:38:36 -0600

    Worked on formatting and content for resume page

--- src/Resume.js
+++ src/Resume.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Resume.css';
 import portraitImg from "./images/ColbyNelsonPortrait.jpg";
+import byuLogo from "./images/BYU-Logo-sq.png";
+import churchLogo from "./images/churchLogo.jpg";
 
 class Resume extends React.Component {
     render() {
@@ -20,68 +22,135 @@ class Resume extends React.Component {
                         </div>
                     </div>
                     <div className="col-sm-12 col-md-9">
-                        <span className="resumeheading">Education</span>
-                        <hr className="hrskills" />
-                        <p>
-                            <span className="bold">Brigham Young University</span><br />
-                            Bachelor of Science Information Systems<br />
-                            April 2020<br />
-                            Provo, UT
-                        </p>
+                        <h3 className="resumeheading">Education</h3>
+                        <hr className="resumeHr" />
+                        <div className="row">
+                            <div className="col-sm-12 col-md-1">
+                                <img className="resumeLogo" src={byuLogo} alt="Brigham Young University Logo"/>
+                            </div>
+                            <div className=" col-sm-12 col-md-11 jobDescription">
+                                <p>
+                                Bachelor of Science &mdash; Information Systems<br />
+                                Brigham Young University Marriott School of Business<br />
+                                April 2021 | Provo, UT
+                                </p>
+                            </div>
+                            <div className="col-sm-12 col-md-1">
+                                <img className="resumeLogo" src={byuLogo} alt="Brigham Young University Logo"/>
+                            </div>
+                            <div className=" col-sm-12 col-md-11 jobDescription">
+                                <p>
+                                Master of Science &mdash; Information Systems<br />
+                                Brigham Young University Marriott School of Business<br />
+                                April 2021 | Provo, UT
+                                </p>
+                            </div>
+                        </div>
+                            
                         
-                        <span className="resumeheading">Experience </span>
-                        <hr className="hrskills" />
-                        <ul>
-                            <li> 
-                                <span className="bold">The Church of Jesus Christ of Latter-day Saints, Quality Assurance Engineer, May 2019 – Aug 2019, Riverton, UT</span>
-                                <ul>
-                                    <li>Plan and develop automated C# testing suite for international student loan program</li>
-                                    <li>Participate in Scrum-style Agile development team</li>
-                                    <li>Increased number of automated tests by nearly 50% in test suite from 42 to 59</li>
-                                </ul>
-                            </li>
-                            <li> 
-                                <span className="bold">BYU School of Accountancy Hub Technical Team, Web Developer, Feb 2019 – Present, Provo, UT</span>
-                                <ul>
+                        <h3 className="resumeheading">Experience </h3>
+                        <hr className="resumeHr" />
+                        <div className="jobDiv row">
+                            <div className="col-sm-12 col-md-1">
+                                <img className="resumeLogo" src={byuLogo} alt="Brigham Young University Logo"/>
+                            </div>
+                            <div className=" col-sm-12 col-md-11 jobDescription">
+                                <p>
+                                Web Developer<br />
+                                BYU School of Accountancy Hub Technical Team<br />
+                                Feb 2019&ndash;Present | Provo, UT
+                                </p>
+                                <ul className="resumeList">
                                     <li>Redesign and customize <a href="https://internationalhub.org">InternationalHub.org</a> with Wordpress tools</li>
                                     <li>Maintain Hub websites (<a href="https://ipohub.org">IPOhub.org</a>, <a href="https://revenuehub.org">RevenueHub.org</a>) updated with current content</li>
                                     <li>Build SEO solutions and increase accessibility</li>
                                 </ul>
-                            </li>
-                            <li> 
-                                <span className="bold">BYU Campus Accommodations, Financial Specialist, Sep 2018 – Dec 2018, Provo, UT</span>
-                                <ul>
+                            </div>
+                        </div>
+                        <div className="jobDiv row">
+                            <div className="col-sm-12 col-md-1">
+                                <img className="resumeLogo" src={churchLogo} alt="Church of Jesus Christ of Latter-day Saints Logo"/>
+                            </div>
+                            <div className="col-sm-12 col-md-11 jobDescription">
+                                <p>
+                                Quality Assurance Engineer Intern<br />
+                                The Church of Jesus Christ of Latter-day Saints<br />
+                                May&ndash;Aug 2019 | Riverton, UT
+                                </p>
+                                <ul className="resumeList">
+                                    <li>Planned and developed automated C# testing suite for international student loan program</li>
+                                    <li>Participated in Scrum-style Agile development team</li>
+                                    <li>Increased number of automated tests by nearly 50% in test suite from 42 to 59</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="jobDiv row">
+                            <div className="col-sm-12 col-md-1">
+                                <img className="resumeLogo" src={byuLogo} alt="Brigham Young University Logo"/>
+                            </div>
+                            <div className=" col-sm-12 col-md-11 jobDescription">
+                                <p>
+                                Financial Specialist<br />
+                                BYU Campus Accommodations<br />
+                                Sep&ndash;Dec 2018 | Provo, UT
+                                </p>
+                                <ul className="resumeList">
                                     <li>Act as customer service representative addressing residents’ concerns and complaints</li>
                                     <li>Created live Tableau report saving 30 minutes/day of employee time</li>
                                 </ul>
-                            </li>
-                            <li> 
-                                <span className="bold">BriCon Construction, Inc., Commercial Construction Worker, May 2018-Jul. 2018, Nampa, ID</span>
-                                <ul>
+                            </div>
+                        </div>
+                        <div className="jobDiv row">
+                            <div className="col-sm-12 col-md-1">
+                                <img className="resumeLogo" src={churchLogo} alt="Church of Jesus Christ of Latter-day Saints Logo"/>
+                            </div>
+                            <div className=" col-sm-12 col-md-11 jobDescription">
+                                <p>
+                                Commercial Construction Worker<br />
+                                BriCon Construction, Inc.<br />
+                                May&ndash;Jul 2018 | Nampa, ID
+                                </p>
+                                <ul className="resumeList">
                                     <li>Install and repair city and county concrete infrastructure</li>
                                     <li>Worked with eight team members to meet deadlines and manage projects</li>
                                 </ul>
-                            </li>
-                            <li>
-                                <span className="bold">Billings Parks and Recreation, Lead Counselor, May 2017-Aug. 2017, Billings, MT</span>
-                                <ul>
-                                    <li>Planned and executed summer camp activities allowing operation of 3 simultaneous camps</li>
-                                    <li>Led team of eight counselors improving risk avoidance and employee scheduling</li>
+                            </div>
+                        </div>
+                        <div className="jobDiv row">
+                            <div className="col-sm-12 col-md-1">
+                                <img className="resumeLogo" src={churchLogo} alt="Church of Jesus Christ of Latter-day Saints Logo"/>
+                            </div>
+                            <div className=" col-sm-12 col-md-11 jobDescription">
+                                <p>
+                                Lead Camp Counselor<br />
+                                Billings Parks and Recreation<br />
+                                May&ndash;Aug 2017 | Billings, MT
+                                </p>
+                                <ul className="resumeList">
+                                    <li>I planned and executed summer camp activities allowing operation of 3 simultaneous camps.</li>
+                                    <li>I led a team of eight counselors improving risk avoidance and employee scheduling.</li>
                                 </ul>
-                            </li>
-                            <li>
-                                <span className="bold">The Church of Jesus Christ of Latter-Day Saints, Volunteer Representative, Aug. 2014-Aug. 2016, Santiago, Chile</span>
-                                <ul>
-                                    <li>Lead team of 10 in setting and meeting goals</li>
-                                    <li>Keep reliable financial records as financial secretary</li>
+                            </div>
+                        </div>
+                        <div className="jobDiv row">
+                            <div className="col-sm-12 col-md-1">
+                                <img className="resumeLogo" src={churchLogo} alt="Church of Jesus Christ of Latter-day Saints Logo"/>
+                            </div>
+                            <div className="col-sm-12 col-md-11 jobDescription">
+                                <p>
+                                Volunteer Representative<br />
+                                The Church of Jesus Christ of Latter-Day Saints<br />
+                                Aug 2014&ndash;Aug 2016 | Santiago, Chile
+                                </p>
+                                <ul className="resumeList">
+                                    <li>I led a team of 10 in setting and meeting goals.</li>
+                                    <li>I developed processes for maintaining reliable financial records as a financial secretary.</li>
                                 </ul>
-                            </li>
-                        </ul>
-                        <span className="resumeheading skills">Skills</span>
-                        
-                        <hr className="hrskills" />
-                        <ul>
-                            <li><span className="bold">Information Systems Skills:</span> <br /> 
+                            </div>
+                        </div>
+                        <h3 className="resumeheading">Education</h3>
+                        <hr className="resumeHr" />
+                        <b>Information Systems Skills:</b> <br /> 
                                 <ul>
                                     <li><span className="bold"> Experienced:</span>
                                     Python; PHP; JavaScript; Windows App GUI; Structured Query Language (MSSQL, Oracle, PostgreSQL); Azure Machine Learning Studio; Tableau; Advanced Excel Tools (Solver, Pivot Tables, Pivot Charts, Statistics Analysis); Excel Automation (VBA); UML Diagramming; Windows Server Management (IIS, DNS, AD)
@@ -90,10 +159,8 @@ class Resume extends React.Component {
                                         C#; MVC Web Development; HTML; CSS; Wordpress
                                     </li>
                                 </ul>
-                            </li>
-                            <li><span className="bold">Language Skills:</span> Fluent in Spanish, Conversational in French</li>
-                            <li><span className="bold">Achievements:</span> <a href="https://is415machinelearningnelson.azurewebsites.net/Default.aspx">Azure Machine Learning Example (Contact Me Through LinkedIn for Authentication Details)</a></li>
-                        </ul>
+                        <b>Language Skills:</b> Fluent in Spanish, Conversational in French <br />
+                        <b>Achievements:</b> <a href="https://is415machinelearningnelson.azurewebsites.net/Default.aspx">Azure Machine Learning Example (Contact Me Through LinkedIn for Authentication Details)</a>
                     </div>
                 </div>
             </div>
